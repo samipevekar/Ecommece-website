7ae969b0b8c533c9ccd743a87b86c22a02696b37
sami pevekar <sami@sam.com>
2023-12-13 17:46:53 +0530

    all file are updated

--- app.js
+++ app.js
@@ -126,3 +126,15 @@ menuItems.forEach((item,index)=>{
     size.style.color = "white";
   });
 });
+
+
+const productButton=document.querySelector(".productButton")
+const payment = document.querySelector(".payment")
+const close = document.querySelector(".close")
+
+productButton.addEventListener("click",()=>{
+  payment.style.display='flex'
+})
+close.addEventListener("click",()=>{
+  payment.style.display='none'
+})
